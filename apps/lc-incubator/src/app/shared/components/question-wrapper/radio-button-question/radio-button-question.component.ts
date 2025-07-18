90cbac7decb1188641556c0f0c2f8cfe29f28ba7
Shant Derounian <shantderounian@gmail.com>
2025-07-18 14:27:00 +0200

    Fixed styling of radio button

--- apps/lc-incubator/src/app/shared/components/question-wrapper/radio-button-question/radio-button-question.component.ts
+++ apps/lc-incubator/src/app/shared/components/question-wrapper/radio-button-question/radio-button-question.component.ts
@@ -1,27 +1,30 @@
 import { Component, Input } from '@angular/core'
-import { NgForOf } from '@angular/common'
 import { MatIcon } from '@angular/material/icon'
 import { Question } from '../../../services/questions.service'
 import { AnswerService } from '../../../services/answer.service'
 import { MatTooltip } from '@angular/material/tooltip'
+import { MatRadioButton, MatRadioChange, MatRadioGroup } from '@angular/material/radio';
 
 @Component({
   selector: 'app-radio-button-question',
   templateUrl: './radio-button-question.component.html',
   styleUrls: ['./radio-button-question.component.scss'],
-  imports: [NgForOf, MatIcon, MatTooltip],
+  imports: [MatIcon, MatTooltip, MatRadioGroup, MatRadioButton],
 })
 export class RadioButtonQuestionComponent {
-  @Input() question?: Question
+  @Input() question?: Question;
 
   constructor(private answerService: AnswerService) {}
 
-  radioButtonSelected($event: Event) {
-    const value = ($event.target as HTMLInputElement).value
-    this.answerService.addOrUpdateAnswer({ questionId: this.question?.id || '', value })
+  radioButtonSelected($event: MatRadioChange) {
+    const value = $event.value;
+    this.answerService.addOrUpdateAnswer({
+      questionId: this.question?.id || '',
+      value,
+    });
   }
 
   getTooltip(q: any): string {
-    return q
+    return q;
   }
 }
