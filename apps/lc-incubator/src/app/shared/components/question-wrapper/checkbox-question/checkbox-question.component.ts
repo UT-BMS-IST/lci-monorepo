0146ad5c28c74870d3c19b0b7a169edb4c675cba
Shant Derounian <shantderounian@gmail.com>
2025-07-18 14:19:17 +0200

    More styling fixes

--- apps/lc-incubator/src/app/shared/components/question-wrapper/checkbox-question/checkbox-question.component.ts
+++ apps/lc-incubator/src/app/shared/components/question-wrapper/checkbox-question/checkbox-question.component.ts
@@ -1,39 +1,42 @@
-import { Component, Input } from '@angular/core'
-import { NgForOf, NgIf } from '@angular/common'
-import { MatIcon } from '@angular/material/icon'
-import { MatTooltip } from '@angular/material/tooltip'
-import { Question } from '../../../services/questions.service'
-import { AnswerService } from '../../../services/answer.service'
+import { Component, Input } from '@angular/core';
+import { NgForOf, NgIf } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { Question } from '../../../services/questions.service';
+import { AnswerService } from '../../../services/answer.service';
+import { MatCheckboxModule, MatCheckboxChange } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-checkbox-question',
   templateUrl: './checkbox-question.component.html',
   styleUrls: ['./checkbox-question.component.scss'],
-  imports: [NgForOf, NgIf, MatIcon, MatTooltip],
+  imports: [NgForOf, NgIf, MatIconModule, MatTooltipModule, MatCheckboxModule],
 })
 export class CheckboxQuestionComponent {
-  @Input() question?: Question
-  selectedValues: string[] = []
+  @Input() question?: Question;
+  selectedValues: string[] = [];
 
   constructor(private answerService: AnswerService) {}
 
-  checkboxSelected($event: Event) {
-    const checkbox = $event.target as HTMLInputElement
-    const value = checkbox.value
+  checkboxSelected(event: MatCheckboxChange): void {
+    const value = event.source.value;
 
-    if (checkbox.checked) {
-      this.selectedValues.push(value)
+    if (event.checked) {
+      this.selectedValues.push(value);
     } else {
-      const index = this.selectedValues.indexOf(value)
-      if (index !== -1) {
-        this.selectedValues.splice(index, 1)
+      const index = this.selectedValues.indexOf(value);
+      if (index > -1) {
+        this.selectedValues.splice(index, 1);
       }
     }
 
-    this.answerService.addOrUpdateAnswer({ questionId: this.question?.id || '', values: this.selectedValues })
+    this.answerService.addOrUpdateAnswer({
+      questionId: this.question?.id || '',
+      values: this.selectedValues,
+    });
   }
 
   getTooltip(q: any): string {
-    return q
+    return q;
   }
 }
