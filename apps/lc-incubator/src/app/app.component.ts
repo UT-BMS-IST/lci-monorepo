debc414f558759fbd4e3d37c2a71d659350e0f34
Shant Derounian <shantderounian@gmail.com>
2026-05-19 16:52:47 +0200

    Refactor welcome component for improved translation support and UI clarity

--- apps/lc-incubator/src/app/app.component.ts
+++ apps/lc-incubator/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatToolbar } from '@angular/material/toolbar';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
-  imports: [RouterModule, MatToolbar],
+  imports: [RouterModule, MatToolbar, TranslatePipe],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
