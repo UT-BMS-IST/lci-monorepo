8d05d2df77547c9bbc3c0bee2cdd29804d551c90
Shant Derounian <shantderounian@gmail.com>
2026-05-20 10:23:32 +0200

    Update questions YAML URL to use raw GitHub link for dynamic loading

--- apps/lc-incubator/src/app/shared/services/questions.service.ts
+++ apps/lc-incubator/src/app/shared/services/questions.service.ts
@@ -6,7 +6,6 @@ import {
   Observable,
   of,
   shareReplay,
-  tap,
 } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { load as yamlLoad } from 'js-yaml';
@@ -23,7 +22,7 @@ export class QuestionsService {
   //     catchError(() => of([])),
   //     shareReplay({ bufferSize: 1, refCount: true })
   //   );
-  questionsYamlUrl = `questions.yaml`;
+  questionsYamlUrl = `https://raw.githubusercontent.com/UT-BMS-IST/lci-monorepo/main/apps/lc-incubator/public/questions.yaml?v=${Date.now()}`;
   private questionnaireSteps$: Observable<QuestionnaireStep[]> = this.http
     .get(this.questionsYamlUrl, { responseType: 'text' })
     .pipe(
